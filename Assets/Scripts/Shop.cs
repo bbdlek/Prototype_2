b0671ef652b8cfb85bf0c2f4e09c17997c35be4e
김태현 <th2252@gmail.com>
2021-08-27 15:09:13 +0900

    Inventory 개선
    Inventory 정보 전달을 Inventory.LateUpdate에서 해주던 것을 TowerManager에서 Inventory.GetSelectedTower()를 호출하는 것으로 수정
    
    타워 설치할 때 토글은 없어져도 _tower[]에서 사용된 타워가 사라지지 않는 것을 수정
    
    _toggle 과 _tower를 둘 다 List로 통일
    
    중복된 코드 약간의 개선

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MakeShoppingList()
+    {
+        // 랜덤 아이템 생성하여 상점에 진열
+    }
+
+    public void TryPurchase()
+    {
+        // 충분한 가루 / 인벤토리 공간이 있는 지 확인
+
+        // 있다면 구매진행
+    }
+
+
+
+
+}
